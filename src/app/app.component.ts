7b4ea7660f389c3b98144af7e9b65074766e7bf4
juliadelua <juliagatoh@gmail.com>
2022-03-25 20:32:36 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  array = [];
+  item = '';
+
+  addItem() {
+    this.array.push(this.item);
+  }
+
+  removeItem(i: number) {
+    this.array.splice(i, 1);
+  }
 }
