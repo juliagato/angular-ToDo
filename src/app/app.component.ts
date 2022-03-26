050dd9b1bf5703d09979304ad4e4245d012014da
juliadelua <juliagatoh@gmail.com>
2022-03-25 21:25:51 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,34 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   array = [];
+  storage = [];
   item = '';
 
   addItem() {
-    this.array.push(this.item);
+    if (this.item !== '') {
+      this.array.push(this.item);
+      let item_number = this.array.length.toString();
+      localStorage.setItem(item_number, this.item);
+      this.item = '';
+    }
   }
 
   removeItem(i: number) {
     this.array.splice(i, 1);
+  }
+
+  getStored() {
+    const items = { ...localStorage };
+    let storage = [];
+    for (let i in items) {
+      if (i !== 'editorLastConnected') {
+        storage.push(' ' + localStorage.getItem(i));
+      }
+    }
+    return storage;
+  }
+
+  clearStored() {
+    localStorage.clear();
   }
 }
